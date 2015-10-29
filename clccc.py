a4e090def2b3d296438d3047c463d093cbf2d0e8
Todd Hepler <thepler@gmail.com>
2015-10-29 13:37:57 -0500

    bit of a design change
    the type of the employee will be text in the object
    easier to display
    added Department class and basic test

--- clccc.py
+++ clccc.py
@@ -8,15 +8,35 @@ class Employee(object):
         allocation: warranted allocation for this employee
     """
 
-    def __init__(self, name="", allocation=0, staff=[]):
+    # XXX use a set instead of a list to make sure no duplicates?
+
+    def __init__(self, etype="", name="", allocation=0, staff=[]):
         self.name = name
         self.allocation = allocation
         self.staff = staff
+        self.etype = etype
+
+    def add_staff(self, more=[]):
+        self.staff.extend(more)
 
     def sum_allocation(self):
         return self.allocation + sum(map(lambda e: e.sum_allocation(), self.staff))
 
 
-class Manager(Employee):
-    pass
+class Department(object):
+    """A department at CenturyLink
+
+    Attributes:
+        name: name of this department
+        staff: list of employees in this department
+    """
+    def __init__(self, name="", staff=[]):
+        self.name = name
+        self.staff = staff
+
+    def add_staff(self, more=[]):
+        self.staff.extend(more)
+
+    def sum_allocation(self):
+        return sum(map(lambda e: e.sum_allocation(), self.staff))
 
--- test_clccc.py
+++ test_clccc.py
@@ -4,15 +4,29 @@ def func(x):
 def test_answer():
     assert func(3) == 4
 
-from clccc import Employee, Manager
+from clccc import Employee, Department
 
-e = Employee('John Doe', 1000)
-assert e.name == 'John Doe'
-assert e.allocation == 1000
-assert e.sum_allocation() == 1000
+def test_simple():
+    d = Employee('Developer', 'John Doe', 1000)
+    assert d.name == 'John Doe'
+    assert d.etype == 'Developer'
+    assert d.allocation == 1000
+    assert d.sum_allocation() == 1000
+    m = Employee('Manager', 'John Doe2', 300, [d])
+    assert m.name == 'John Doe2'
+    assert m.etype == 'Manager'
+    assert m.allocation == 300
+    assert m.sum_allocation() == 1300
+    d = Department('Yipee', [m])
+    assert d.name == 'Yipee'
+    assert d.sum_allocation() == 1300
 
-m = Manager('John Doe', 1000, [e])
-assert m.name == 'John Doe'
-assert m.allocation == 1000
-assert m.sum_allocation() == 2000
+def test_example():
+    pass
+#    allocation = {'Developer':1000, 'QA Tester':500, 'Manager':300} 
+#    staff = {'Manager B': 'Manager A', 'Developer A': 'Manager B', 'QA Tester
+#    e = ['Manager', 'Joe Man', 300]
+#    p = Employee(e)
+#    assert p.name == 'Joe Man'
+#
 
